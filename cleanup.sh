7b44b85b11f330a08e453a323dd4c1eec8808ee0
Joel Nitta <joelnitta@gmail.com>
2018-03-03 21:16:54 +0900

    add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+# deletes intermediate files. 
+# these can't be recovered, so use with care!
+
+# various intermediate latex files
+rm -f *.aux
+rm -f *.bbl
+rm -f *.blg
+rm -f *.dvi
+rm -f *.log
+
+# R output
+rm -f results.RData
+rm -f PCA.pdf
+rm -f microclimate.pdf
+
+# etc
+rm -r readme.html
