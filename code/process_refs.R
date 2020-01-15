d1b5dd44b349db660ec2a55d5f359008b8cce42a
Joel Nitta <joelnitta@gmail.com>
2020-01-15 18:04:55 +0900

    Use separate bib and yaml for normal and website refs

--- code/process_refs.R
+++ code/process_refs.R
@@ -6,31 +6,35 @@ library(tidyverse)
 make_ref_list(
   rmd_file = "ms/manuscript.Rmd", 
   raw_bib = "ms/references_raw.bib",
-  final_bib = "ms/references.bib")
+  final_bib = "ms/references.bib",
+  strip_fields = "abstract",
+  exclude = c("Laliberte2011", "Orme2013", "Thiers2019"))
 
 make_ref_list(
   rmd_file = "ms/SI.Rmd", 
   raw_bib = "ms/references_raw.bib", 
-  final_bib = "ms/si_references.bib"
+  final_bib = "ms/references_si.bib",
+  exclude = "FloraNSW2019"
 )
 
 # Make some manual fixes to authors in SI bibliography
 # (these are institutions, so need double brackets to
 # avoid latex thinking they have first and last names)
-read_lines("ms/si_references.bib") %>%
+read_lines("ms/references_si.bib") %>%
   str_replace(
     "National Museum of Nature and Science",
     "\\{National Museum of Nature and Science\\}") %>%
-  str_replace(
-    "Royal Botanic Gardens and Domain Trust",
-    "\\{Royal Botanic Gardens and Domain Trust\\}") %>%
-  write_lines("ms/si_references.bib")
+  write_lines("ms/references_si.bib")
 
-# Make some manual fixes to authors in SI bibliography
-# (these are institutions, so need double brackets to
-# avoid latex thinking they have first and last names)
+# Make some manual fixes to main bibliography
 read_lines("ms/references.bib") %>%
   str_replace(
     "Pteridophyte Phylogeny Group I",
     "\\{Pteridophyte Phylogeny Group I\\}") %>%
+  str_replace(
+    "Adaptive radiation versus 'radiation' and 'explosive diversification'",
+    "Adaptive radiation versus `radiation' and `explosive diversification'") %>%
+  str_replace(
+    "'Sun leaves' and 'shade leaves'",
+    "`Sun leaves' and `shade leaves'") %>%
   write_lines("ms/references.bib")
