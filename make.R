18c45fb43b76d9447549ac1a413fa52de1ef3b46
joelnitta <joelnitta@gmail.com>
2020-02-07 15:00:53 +0900

    Make plan in local environment

--- make.R
+++ make.R
@@ -15,8 +15,13 @@ source("code/setup.R")
 # Update drake settings.
 pkgconfig::set_config("drake::strings_in_dots" = "literals")
 
+# Specify non-global environment
+# to get around taxize and captioner modifying global env
+# (cf https://github.com/ropensci/drake/issues/749)
+envir <- new.env(parent = globalenv())
+
 # Load functions and plans.
-source("code/functions.R")
+source("code/functions.R", local = envir)
 source("code/plan.R")
 
 # Load cache
@@ -26,4 +31,4 @@ canopy_cache <- new_cache("canopy_cache")
 set.seed(1954)
 
 # Make plan.
-make(plan, cache = canopy_cache)
+make(plan, cache = canopy_cache, envir = envir)
